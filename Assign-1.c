b46a62fcb589ebf19d842db0d426f56211191548
BabluSingh7 <bablusinghkb1@gmail.com>
2025-11-19 20:49:16 +0530

    Assign-1.0

--- Assign-1.c
+++ Assign-1.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
-int main()
+int p1main()
 {
     printf("Hello");
+}
+//Q-2
+int p2main()
+{
+    printf("Hello\nStudent");
+}
+//Q-3
+int main()
+{
+    printf("\"MySirG\"");
 }
